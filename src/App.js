ae7cbeeb2448682c4dcee7d753053b657cc6987d
Kevin Ruiz <kevin.ruiz1811@gmail.com>
2023-04-30 15:53:01 -0600

    Gestión del usuario

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
-import { Home } from "./pages/Home";
-import { Detail } from "./pages/Detail";
+import { Home } from "./components/pages/Home";
+import { Detail } from "./components/pages/Detail";
 import { Router } from "@reach/router";
 import { NavBar } from "./components/NavBar";
 import { User } from "./components/pages/User";
 import { Favs } from "./components/pages/Favs";
 import { NotRegisteredUser } from "./components/pages/NotRegisteredUser";
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: false });
-};
+import Context from "./Context";
 
 export const App = () => {
   return (
@@ -23,7 +20,7 @@ export const App = () => {
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
-      <UserLogged>
+      <Context.Consumer>
         {({ isAuth }) =>
           isAuth ? (
             <Router>
@@ -37,7 +34,7 @@ export const App = () => {
             </Router>
           )
         }
-      </UserLogged>
+      </Context.Consumer>
       <NavBar />
     </div>
   );
